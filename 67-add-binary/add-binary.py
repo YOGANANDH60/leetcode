c044543069492846283f947a95c8a5f6c454abb1
Yoganandh <141514980+YOGANANDH60@users.noreply.github.com>
2025-12-09 23:43:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (52.45%) - LeetSync

--- 67-add-binary/add-binary.py
+++ 67-add-binary/add-binary.py
@@ -1,6 +1,6 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         decimal = int(a,2) + int(b,2)
-        binary = bin(decimal)
-        # return (bin(decimal)[2:])
-        return binary[2:]
+        # binary = bin(decimal)
+        # return binary[2:]
+        return (bin(decimal)[2:])
