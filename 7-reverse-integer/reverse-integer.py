276f85917a04c4a16bc2b1c318f9e33badcac3ff
Yoganandh <141514980+YOGANANDH60@users.noreply.github.com>
2025-12-29 21:37:58 +0530

    Time: 21 ms (31.30%) | Memory: 12.5 MB (7.93%) - LeetSync

--- 7-reverse-integer/reverse-integer.py
+++ 7-reverse-integer/reverse-integer.py
@@ -4,25 +4,19 @@ class Solution(object):
         :type x: int
         :rtype: int
         """
-        if num < 0:
-            n = -num
-            reverse = 0
-            while n>0:
-                last = n%10
-                reverse = reverse*10+last
-                n = n//10
-            reverse = -reverse
-            # return -reverse
-            if reverse < -2**31 or reverse > 2**31 - 1:
-                return 0
-            return reverse
-        else:
-            reverse = 0
-            while num>0:
-                last = num%10
-                reverse = reverse*10+last
-                num = num//10
-            # return reverse
-            if reverse < -2**31 or reverse > 2**31 - 1:
-                return 0
-            return reverse
+        sign = -1 if num < 0 else 1
+        num = abs(num)
+
+        reverse = 0
+        while num > 0:
+            last = num % 10
+            reverse = reverse * 10 + last
+            num //= 10
+
+        reverse *= sign
+
+        # 32-bit signed integer check
+        if reverse < -2**31 or reverse > 2**31 - 1:
+            return 0
+
+        return reverse
